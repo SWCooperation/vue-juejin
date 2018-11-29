79c234e5f773b7920a8229d99cd8d16c09981df0
songzeng <songzeng62@gmail.com>
2018-11-29 22:32:05 +0800

    增加小测下拉刷新

--- src/components/books/books.vue
+++ src/components/books/books.vue
@@ -1,73 +1,101 @@
 <template>
   <div class="books" ref="books" v-loading="loading">
-    <ul class="list-wrapper">
-      <li class="list" v-for="(item, index) in list" :key="index">
-        <img class="img" :src="item.url" alt="">
-        <div>
-          <span>{{item.title}}</span>
-        </div>
-        <span>{{item.price}}</span>
-      </li>
-    </ul>
+    <scroll
+      ref="scroll"
+      :data="list"
+      :scrollbar="scrollbar"
+      :pullDownRefresh="pullDownRefresh"
+      :pullUpLoad="pullUpLoad"
+      @pullingDown="onPullingDown"
+      @pullingUp="onPullingUp">
+      <ul class="list-wrapper">
+        <li class="list" v-for="(item, index) in list" :key="index">
+          <img class="img" :src="item.url" alt="">
+          <div>
+            <span>{{item.title}}</span>
+          </div>
+          <span>{{item.price}}</span>
+        </li>
+      </ul>
+    </scroll>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   import {requestParameters} from 'common/js/api';
-  import BScroll from 'better-scroll';
+  // import BScroll from 'better-scroll';
+  import Scroll from 'base/scroll/scroll';
 
   export default {
+    components: {
+      Scroll,
+    },
     data() {
       return {
         loading: false,
         list: [],
+        scrollbar: {
+          fade: true,
+          interactive: false // 1.8.0 新增
+        },
+        pullDownRefresh: {
+          threshold: 90,
+          stop: 40
+        },
+        pullUpLoad: {
+          threshold: 50
+        }
       };
     },
     created() {
-      this._getList();
+      this.pageNum = 1;
+      this.init();
     },
     methods: {
+      // 获取数据列表
       _getList() {
+        return new Promise(resolve => {
+          this.$get(requestParameters.url_getListByLastTime, {
+            ...requestParameters.arg_getListByLastTime,
+            pageNum: this.pageNum,
+          }).then(json => {
+            resolve(json);
+          });
+        });
+      },
+      // 初始化
+      init() {
         this.loading = true;
-        this.$get(requestParameters.url_getListByLastTime, {
-          ...requestParameters.arg_getListByLastTime,
-          pageNum: 1,
-        }).then(json => {
-          console.log(json);
+        this._getList().then(json => {
           this.list = json.d;
-          this.$nextTick(() => {
-            this.scroll = new BScroll(this.$refs.books, {
-              scrollbar: {
-                fade: true,
-                interactive: false,
-              },
-              pullDownRefresh: {
-                threshold: 90,
-                stop: 40
-              },
-              pullUpLoad: {
-                threshold: 50
-              }
-            });
-
-            this.scroll.on('pullingDown', () => {
-              // 刷新数据的过程中，回弹停留在距离顶部还有20px的位置
-              // RefreshData()
-              //   .then((newData) => {
-              //     this.data = newData
-              //     // 在刷新数据完成之后，调用 finishPullDown 方法，回弹到顶部
-              //     this.scroll.finishPullDown()
-              //   })
-              console.log(0);
-              setTimeout(() => {
-                this.scroll.finishPullDown();
-              }, 1000);
-            });
-          })
         }).finally(() => {
           this.loading = false;
         });
       },
+      // 下拉刷新
+      onPullingDown() {
+        this.pageNum = 1;
+        this._getList().then(json => {
+          this.list = json.d;
+        }).catch(() => {
+          this.$refs.scroll.forceUpdate();
+        });
+      },
+      // 上拉加载更多
+      onPullingUp() {
+        this.pageNum++;
+        this._getList().then(json => {
+          if (json.d.length) {
+            this.list = this.list.concat(json.d);
+          } else {
+            // 如果没有新数据
+            this.$refs.scroll.forceUpdate()
+          }
+        }).catch(() => {
+          this.$refs.scroll.forceUpdate();
+        });
+      },
+
     },
   };
 </script>
@@ -78,7 +106,6 @@
     height: 100%
     overflow: hidden
     .list-wrapper
-      height: 120%
       .list
         display: flex
         .img
--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -4,11 +4,11 @@
       <!-- <img src="" alt=""> -->
     </div>
     <div class="tab">
- 
-        <div class="tab-li" v-for="(item,index) in tabList" :key="index">
-          <span>{{item.name}}</span>
-        </div>
-      
+
+      <div class="tab-li" v-for="(item,index) in tabList" :key="index">
+        <span>{{item.name}}</span>
+      </div>
+
     </div>
     <ul>
       <li class="list" v-for="(item,index) in list" :key="index">
@@ -20,32 +20,32 @@
 
 <script type="text/ecmascript-6">
   export default {
-    data(){
-      return{
-        tabList:[
-          {name:'本周最热',url:'',img:''},
-          {name:'收藏夹',url:'',img:''},
-          {name:'活动',url:'',img:''}
+    data() {
+      return {
+        tabList: [
+          {name: '本周最热', url: '', img: ''},
+          {name: '收藏夹', url: '', img: ''},
+          {name: '活动', url: '', img: ''}
         ],
-        list:[]
+        list: []
       };
     },
   };
 </script>
 
 <style lang="stylus" type="text/stylus" scoped>
-   .search
-      background :#d8d8d8
-      height: 100%
-      overflow: hidden
-      .tab
-        width 100%
-        height 80px
-        line-height 80px
-        background #fff
-        display flex
-        .tab-li
-          flex 1
-          display inline-block
-          text-align center
+  .search
+    background: #d8d8d8
+    height: 100%
+    overflow: hidden
+    .tab
+      width 100%
+      height 80px
+      line-height 80px
+      background #fff
+      display flex
+      .tab-li
+        flex 1
+        display inline-block
+        text-align center
 </style>
